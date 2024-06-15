66c8d9f73d29a61411f4ece35a41dedf8cc5c779
S. Joshua Swamidass <swamidass@gmail.com>
2024-06-15 11:32:27 -0500

    moved to data folder

--- wsiml/taesd/__init__.py
+++ wsiml/taesd/__init__.py
@@ -1,9 +1,16 @@
 import onnxruntime as rt
 
+import pooch
+
+
+model_path = pooch.retrieve(
+    "https://github.com/swamidass/wsi/raw/master/data/taesd-encoder.onnx",
+    known_hash="md5:1e64ceb4eb98bef77202c91f6584e50a",
+)
 
 
 class TAESD:
-    def __init__(self, model_path):
+    def __init__(self, model_path=model_path):
         self.model_path = model_path
         self.model = rt.InferenceSession(model_path)
         self.input_name = self.model.get_inputs()[0].name
@@ -11,3 +18,4 @@ class TAESD:
 
     def predict(self, x):
         return self.model.run([self.output_name], {self.input_name: x})[0]
+    
